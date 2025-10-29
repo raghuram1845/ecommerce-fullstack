7976e94fbcb06dfc481fbe213ebeda9912ffc9fe
Raghu <raghuramroyya2022@gmail.com>
2025-10-30 01:02:10 +0530

    bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import validator from "validator";
-import bcrypt from "bcrypt"
+import bcryptjs from "bcryptjs"
 import jwt from 'jsonwebtoken'
 import userModel from "../models/userModel.js";
 
@@ -20,7 +20,7 @@ const loginUser = async (req, res) => {
             return res.json({ success: false, message: "User doesn't exists" })
         }
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcryptjs.compare(password, user.password);
 
         if (isMatch) {
 
@@ -59,8 +59,8 @@ const registerUser = async (req, res) => {
         }
 
         // hashing user password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
 
         const newUser = new userModel({
             name,
